70fd570f405509ce15e5d097a14b76cfb031416c
uprun <geraclion@gmail.com>
2020-06-02 00:59:01 +0300

    added animation script and basic model of house

--- house.gd
+++ house.gd
@@ -6,7 +6,7 @@ extends KinematicBody
 # var b: String = "text"
 
 var velocity = Vector3.ZERO
-var jumpCountLeft = 3
+var jumpCountLeft = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
---
+++ low-poly-house-build-process.gd
@@ -0,0 +1,33 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+var initial_translation: Vector3
+export var start_from_translation: Vector3
+
+export var progress_per_second = 100.0
+export var show_and_animate_after_seconds = 0.0
+
+var progress = 0
+var time_passed = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	initial_translation = translation
+	visible = false
+	start_from_translation = initial_translation + start_from_translation
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if(time_passed >= show_and_animate_after_seconds):
+		visible = true
+		translation = (initial_translation - start_from_translation) * progress / 100 + start_from_translation
+		progress += progress_per_second * delta
+		progress = min(100, progress)
+	time_passed += delta
+
